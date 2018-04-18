664531d8b505dff132ab5fb68b06efee9eb3530f
zhangyun <zhangyun@AppledeMacBook-Pro.local>
2018-04-18 19:49:04 +0800

    aiohttp

--- awesome/app.py
+++ awesome/app.py
@@ -3,6 +3,7 @@ from aiohttp import web
 import time,threading
 import mysql.connector
 import asyncio 
+from aiohttp import web
 
 def Loop():
     print('thread %s is running' % threading.current_thread().name)
@@ -88,12 +89,29 @@ def wget(host):
 
 
 
+async def index(request):
+    await asyncio.sleep(0.5)
+    return web.Response(body=b'<h1>Index</h1>')
+
+async def hello(request):
+    await asyncio.sleep(0.5)
+    text = '<h1>hello ,%s !</h1>' % request.match_info['name']
+    return web.Response(body=text.encode('utf-8'))
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    app.router.add_route('GET','/hello/{name}',hello)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',8000)
+    print('server started at http://127.0.0.1:8000')
+    return srv
+
+
 
 if __name__=='__main__':
     loop = asyncio.get_event_loop()
-    tasks = [wget(host) for host in ['www.sina.cn','www.sohu.com','www.163.com']]
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
+    loop.run_until_complete(init(loop))
+    loop.run_forever()
 
     
 
