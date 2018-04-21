7747222381a531d7ed742919d85d9741bfc5055e
zhangyun <zhangyun@AppledeMacBook-Pro.local>
2018-04-21 16:32:28 +0800

    网站爬虫

--- awesome/html_outputer.py
+++ awesome/html_outputer.py
@@ -16,8 +16,8 @@ class HtmlOutputer(object):
         for data in self.datas:
             fout.write('<tr>')
             fout.write('<td>%s</td>' % data['url'])
-            fout.write('<td>%s</td>' % data['title'].encode('utf-8'))
-            fout.write('<td>%s</td>' % data['summary'].encode('utf-8'))
+            fout.write('<td>%s</td>' % data['title'])
+            fout.write('<td>%s</td>' % data['summary'])
             fout.write('</tr>')
         fout.write('</table>')
         fout.write('</body>')
