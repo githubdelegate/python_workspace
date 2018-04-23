26db56cb737fb41fafd1c93810208d2d09368bf3
zhangyun <ostmail@163.com>
2018-04-23 21:03:29 +0800

    part4 can use

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question,Choice
 from django.http import Http404
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -14,9 +15,21 @@ def detail(request,question_id):
    return render(request,'polls/detail.html',{'question':question})
 
 
-def results(request):
-    pass
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
-def vote(request):
-    pass
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':'请选着'
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
     
