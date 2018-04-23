da94ca28d222dd1efa08aa8c7df550e973bff723
zhangyun <ostmail@163.com>
2018-04-23 19:38:54 +0800

    part3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+from django.http import Http404
+
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list }
+    return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+   question = get_object_or_404(Question,pk=question_id)
+   return render(request,'polls/detail.html',{'question':question})
+
+
+def results(request):
+    pass
+
+def vote(request):
+    pass
     
